2cbeeeb7cc9ee28e9b669a1d9afb27e2978b00c7
Lluke18 <lucianlesustefan@gmail.com>
2025-03-31 15:54:49 +0300

    created basic nodes

--- rpgprojekt/Items/Item Scripts/Item.gd
+++ rpgprojekt/Items/Item Scripts/Item.gd
@@ -4,6 +4,6 @@ class_name Item
 
 @export var name: String
 @export var texture: Texture2D
-@export_enum("Weapon", "Armor", "Revitalizer", "Story Item") var type:String
+@export_enum("Weapon", "Armor", "Revitalizer", "Story Item") var type: String
 @export var stack_amnt: int
 @export var item_path: String
---
+++ rpgprojekt/Items/weapon.gd
@@ -0,0 +1,10 @@
+extends Item
+
+class_name Weapon
+
+@export var atk_dmg: float
+
+
+
+func _ready():
+	self.type = "Weapon"
